044ee2e50c9b99be0c5692b5e832667e1e488ab2
panida-pov <panida.pov@gmail.com>
2023-08-02 16:58:13 +0900

    Update app Routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,14 @@
 import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
 import Header from "../components/Header/Header";
 import SignIn from "../components/SignIn/SignIn";
+import Playlists from "../components/Playlists/Playlists";
+import CreatePlaylist from "../components/CreatePlaylist/CreatePlaylist"
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Header/>} >
-    <Route index element={<SignIn/>}></Route>
+    <Route index element={<Playlists/>}></Route>
+    <Route path="create-playlist" element={<CreatePlaylist/>}></Route>
+    <Route path="sign-in" element={<SignIn/>}></Route>
   </Route>
 ));
 

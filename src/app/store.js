fca9bd1b9a36a4dd9242cc2b68686068cf037864
panida-pov <panida.pov@gmail.com>
2023-08-07 16:42:51 +0900

    Add searchTerm and playlists states to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchResultsReducer from "../components/SearchResults/searchResultsSlice";
 import newPlaylistReducer from "../components/NewPlaylist/newPlaylistSlice";
+import searchBarReducer from "../components/SearchBar/searchBarSlice";
+import playlistsReducer from "../components/Playlists/playlistsSlice"
 
 export const store = configureStore({
   reducer: {
     searchResults: searchResultsReducer,
     newPlaylist: newPlaylistReducer,
+    searchTerm: searchBarReducer,
+    playlists: playlistsReducer
   }
 });

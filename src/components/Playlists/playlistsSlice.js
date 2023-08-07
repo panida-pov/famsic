3a92f28869d49f23f22c8579e57d3ebe00cb684e
panida-pov <panida.pov@gmail.com>
2023-08-07 16:35:22 +0900

    Create playlistsSlice

---
+++ src/components/Playlists/playlistsSlice.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { Spotify } from "../../util/Spotify";
+
+export const loadPlaylists = createAsyncThunk(
+  'playlists/loadPlaylists',
+  async () => {
+    return await Spotify.getPlaylists();
+  }
+);
+
+const options = {
+  name: 'playlists',
+  initialState: {
+    playlists: [],
+    isLoading: false,
+    failedToLoad: false
+  },
+  reducers: {
+  },
+  extraReducers: {
+    [loadPlaylists.pending]: (state, action) => {
+      state.isLoading = true;
+      state.failedToLoad = false;
+    },
+    [loadPlaylists.fulfilled]: (state, action) => {
+      state.playlists = action.payload;
+      state.isLoading = false;
+      state.failedToLoad = false;
+    },
+    [loadPlaylists.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.failedToLoad = true;
+    }
+  }
+}
+
+const playlistsSlice = createSlice(options);
+
+export const selectPlaylists = (state) => state.playlists.playlists;
+export default playlistsSlice.reducer;

66688f0215012c720bbaa00791e8e559bcac1ca4
panida-pov <panida.pov@gmail.com>
2023-08-07 16:33:41 +0900

    Add event handlers for SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,14 +1,63 @@
 import styles from './SearchBar.module.css'
-import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FaMagnifyingGlass, FaXmark } from "react-icons/fa6";
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMusic } from '../SearchResults/searchResultsSlice';
+import { 
+  selectSearchMusic, 
+  selectSearchPlaylist, 
+  setSearchMusic, 
+  setSearchPlaylist, 
+  clearSearch
+} from './searchBarSlice'
 
-function SearchBar() {
+function SearchBar({type}) {
+  const dispatch = useDispatch();
+  const music = useSelector(selectSearchMusic);
+  const playlist = useSelector(selectSearchPlaylist);
+  
+  const searchBarType = {
+    searchMusic: {
+      searchTerm: music,
+      placeholder: 'Search for music',
+      handleSubmit(event) {
+        event.preventDefault();
+        dispatch(fetchMusic(music));
+      },
+      handleChange({target}) {
+        dispatch(setSearchMusic(target.value));
+      }
+    },
+    searchPlaylist: {
+      searchTerm: playlist,
+      placeholder: 'Search your playlist',
+      handleSubmit(event) {
+        event.preventDefault();
+      },
+      handleChange({target}) {
+        dispatch(setSearchPlaylist(target.value));
+      }
+    }
+  }
+
   return (
-    <form className={styles.SearchBar}>
-      <input placeholder="SEARCH"></input>
-      <button><FaMagnifyingGlass /></button>
+    <form className={styles.SearchBar} onSubmit={searchBarType[type].handleSubmit}>
+      <button type="submit">
+        <FaMagnifyingGlass />
+      </button>
+      <input 
+        placeholder={searchBarType[type].placeholder || "SEARCH"} 
+        onChange={searchBarType[type].handleChange} 
+        value={searchBarType[type].searchTerm}>
+      </input>
+      <button 
+        className={styles.clearButton} 
+        type="button" 
+        onClick={() => dispatch(clearSearch())}
+      >
+        <FaXmark />
+      </button>
     </form>
   );
-
 }
 
 export default SearchBar;

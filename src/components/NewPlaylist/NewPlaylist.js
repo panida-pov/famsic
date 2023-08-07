985cbe01ab8ad1e6dbc0a0c7a69fbfd4f9d86383
panida-pov <panida.pov@gmail.com>
2023-08-07 16:37:32 +0900

    Add event handlers to NewPlaylist

--- src/components/NewPlaylist/NewPlaylist.js
+++ src/components/NewPlaylist/NewPlaylist.js
@@ -1,14 +1,35 @@
-import Tracklist from "../Tracklist/Tracklist";
+import Track from "../Track/Track";
 import styles from "./NewPlaylist.module.css";
+import { setName, selectPlaylistName, selectTracks, createPlaylist } from "./newPlaylistSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function NewPlaylist() {
+  const playlistName = useSelector(selectPlaylistName);
+  const tracks = useSelector(selectTracks);
+  const dispatch = useDispatch();
+  
+  const handleCreatePlaylist = () => {
+    dispatch(createPlaylist({
+      name: playlistName,
+      uris: tracks.map(track => track.uri)
+    }));
+  };
+
   return (
     <div className={styles.NewPlaylist}>
-      <input placeholder="ENTER PLAYLIST NAME"/>
+      <input 
+        placeholder="ENTER PLAYLIST NAME" 
+        onChange={({target}) => dispatch(setName(target.value))} 
+        value={playlistName}>
+      </input>
       <div className={styles.Tracklist}>
-        <Tracklist />
+        {tracks.map((track, index) => {
+          return (
+            <Track key={index} track={track} action='remove' />
+          );
+        })}
       </div>
-      <button className={styles.create}>CREATE</button>
+      <button onClick={handleCreatePlaylist} className={styles.create}>CREATE</button>
     </div>
   );
 }

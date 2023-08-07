8a237cb410d11e297e298ff8864d61abb93325e6
panida-pov <panida.pov@gmail.com>
2023-08-07 16:42:04 +0900

    Add Playlist props

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,19 +1,12 @@
 import styles from "./Playlist.module.css";
 
-// ------ Sample album ------
-const album = {
-  image: "https://i.scdn.co/image/ab67616d0000b27313b3e37318a0c247b550bccd",
-  name: "X (Deluxe Edition)"
-}
-// -------------------------
-
-function Playlist() {
+function Playlist({playlist}) {
   return (
     <div className={styles.Playlist}>
       <div className={styles.imgContainer}>
-        <img src={album.image} />
+        <img src={playlist.image} />
       </div>
-      <p>{album.name}</p>
+      <p>{playlist.name}</p>
     </div>
 
   );

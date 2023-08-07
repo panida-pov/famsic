b4ffffe96d21da288e659755aa0f912336778c6f
panida-pov <panida.pov@gmail.com>
2023-08-07 16:37:58 +0900

    Create searchResultsSlice

---
+++ src/components/SearchResults/searchResultsSlice.js
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { Spotify } from "../../util/Spotify";
+
+export const fetchMusic = createAsyncThunk(
+  'searchResults/fetchMusic',
+  async (word) => {
+    return await Spotify.search(word);
+  }
+);
+
+const options = {
+  name: 'searchResults',
+  initialState: {
+    tracks: [],
+    isLoading: false,
+    failedToLoad: false
+  },
+  reducers: {
+    addSearchResults: (state, action) => {
+      return action.payload;
+    }
+  },
+  extraReducers: {
+    [fetchMusic.pending]: (state, action) => {
+      state.isLoading = true;
+      state.failedToLoad = false;
+    },
+    [fetchMusic.fulfilled]: (state, action) => {
+      state.tracks = action.payload;
+      state.isLoading = false;
+      state.failedToLoad = false;
+    },
+    [fetchMusic.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.failedToLoad = true;
+    }
+  }
+}
+
+const searchResultsSlice = createSlice(options);
+
+export const selectSearchResults = (state) => state.searchResults.tracks;
+export const { addSearchResults } = searchResultsSlice.actions;
+export default searchResultsSlice.reducer;

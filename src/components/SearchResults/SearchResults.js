3b328c5e2f98fb8db140a7a12cd6975e35e6cfae
panida-pov <panida.pov@gmail.com>
2023-08-07 16:39:24 +0900

    Map searchResults and render Track

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,12 +1,23 @@
 import SearchBar from '../SearchBar/SearchBar';
-import Tracklist from '../Tracklist/Tracklist';
-import styles from './SearchResults.module.css'
+import Track from '../Track/Track';
+import styles from './SearchResults.module.css';
+import { useSelector } from 'react-redux';
+import { selectSearchResults } from './searchResultsSlice'
+
 function SearchResults() {
+  const searchResults = useSelector(selectSearchResults);
+
   return (
     <div className={styles.SearchResults}>
-      <SearchBar />
+      <SearchBar 
+        type="searchMusic"
+      />
       <div className={styles.Tracklist}>
-        <Tracklist />
+        {searchResults.map((result, index) => {
+          return (
+            <Track key={index} track={result} action='add' />
+          );
+        })}
       </div>
     </div>
   );

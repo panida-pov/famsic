8259b53838054c47080b5a4aa9acc32223e19509
panida-pov <panida.pov@gmail.com>
2023-08-08 18:53:42 +0900

    Add event handlers to Playlist

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,10 +1,25 @@
 import styles from "./Playlist.module.css";
+import { useDispatch } from "react-redux";
+import { setCurrentInfo } from "./playlistSlice";
+import { useNavigate } from "react-router-dom";
 
 function Playlist({playlist}) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    dispatch(setCurrentInfo({
+      name: playlist.name,
+      image: playlist.image,
+      id: playlist.id
+    }));
+    navigate('/playlist');
+  };
+
   return (
     <div className={styles.Playlist}>
       <div className={styles.imgContainer}>
-        <img src={playlist.image} />
+        <img src={playlist.image} alt={playlist.name} onClick={handleClick}/>
       </div>
       <p>{playlist.name}</p>
     </div>

a997874b441c4e4e84a8a71f5d04383993292ee3
panida-pov <panida.pov@gmail.com>
2023-08-08 18:57:12 +0900

    Add currentPlaylist to store

--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,14 @@ import searchResultsReducer from "../components/SearchResults/searchResultsSlice
 import newPlaylistReducer from "../components/NewPlaylist/newPlaylistSlice";
 import searchBarReducer from "../components/SearchBar/searchBarSlice";
 import playlistsReducer from "../components/Playlists/playlistsSlice"
+import playlistReducer from "../components/Playlist/playlistSlice";
 
 export const store = configureStore({
   reducer: {
     searchResults: searchResultsReducer,
     newPlaylist: newPlaylistReducer,
     searchTerm: searchBarReducer,
-    playlists: playlistsReducer
+    playlists: playlistsReducer,
+    currentPlaylist: playlistReducer
   }
 });

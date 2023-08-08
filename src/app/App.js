cdadc2f81bf243ebedeb31625b667b8103055794
panida-pov <panida.pov@gmail.com>
2023-08-08 18:57:49 +0900

    Create /playlist route

--- src/app/App.js
+++ src/app/App.js
@@ -3,10 +3,12 @@ import Header from "../components/Header/Header";
 import SignIn from "../components/SignIn/SignIn";
 import Playlists from "../components/Playlists/Playlists";
 import CreatePlaylist from "../components/CreatePlaylist/CreatePlaylist"
+import PlaylistDetails from "../components/PlaylistDetails/PlaylistDetails";
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Header/>} >
     <Route index element={<Playlists/>}></Route>
+    <Route path="playlist" element={<PlaylistDetails/>}></Route>
     <Route path="create-playlist" element={<CreatePlaylist/>}></Route>
     <Route path="sign-in" element={<SignIn/>}></Route>
   </Route>

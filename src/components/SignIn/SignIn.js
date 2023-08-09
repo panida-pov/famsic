552b9176ae4e14691b4832e92627bf0557baabd1
panida-pov <panida.pov@gmail.com>
2023-08-09 18:59:10 +0900

    Add event handler

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,10 +1,11 @@
-import styles from './SignIn.module.css'
+import { Spotify } from '../../util/Spotify';
+import styles from './SignIn.module.css';
 
 function SignIn() {
   return (
     <div className={styles.SignIn}>
       <img className={styles.logo} src="img/Spotify_Logo_RGB_White.png" alt="log in to spotify" />
-      <button className={styles.button}>LOG IN</button>
+      <button className={styles.button} onClick={() => Spotify.requestToken()}>LOG IN</button>
     </div>
 
   );

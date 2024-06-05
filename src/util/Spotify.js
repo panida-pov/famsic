1d14f3559fd7205ecfa558f092eeb38eeb9efc83
panida-pov <panida.pov@gmail.com>
2024-06-05 16:05:01 +0900

    bug fix: modify getplaylists to handle case when img url is null

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,13 +1,13 @@
-const client_id = 'ENTER YOUR CLIENT ID HERE';
-const domain = 'http://localhost:3000/';
+const client_id = "ENTER YOUR CLIENT ID HERE";
+const domain = "http://localhost:3000/";
 let accessToken;
 let expiresIn;
 
 export const Spotify = {
   requestToken() {
-      window.location = `https://accounts.spotify.com/authorize?response_type=token&client_id=${client_id}&scope=playlist-modify-public&redirect_uri=${domain}`;
+    window.location = `https://accounts.spotify.com/authorize?response_type=token&client_id=${client_id}&scope=playlist-modify-public&redirect_uri=${domain}`;
   },
-  
+
   getAccessToken() {
     if (accessToken) {
       return accessToken;
@@ -17,13 +17,13 @@ export const Spotify = {
     if (accessTokenMatch && expiresInMatch) {
       accessToken = accessTokenMatch[1];
       expiresIn = Number(expiresInMatch[1]);
-      window.setTimeout(() => accessToken = '', expiresIn*1000);
-      window.history.pushState('Access Token', null, '/');
+      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+      window.history.pushState("Access Token", null, "/");
       return accessToken;
     } else {
       window.location = `${domain}sign-in`;
     }
-  }, 
+  },
 
   async search(word) {
     try {
@@ -32,12 +32,12 @@ export const Spotify = {
       const response = await fetch(endpoint, { headers: headers });
       const jsonResponse = await response.json();
       if (jsonResponse) {
-        return jsonResponse.tracks.items.map(item => ({
+        return jsonResponse.tracks.items.map((item) => ({
           name: item.name,
           artist: item.artists[0].name,
           album: item.album.name,
           uri: item.uri,
-          id: item.id
+          id: item.id,
         }));
       }
       return [];
@@ -46,31 +46,39 @@ export const Spotify = {
     }
   },
 
-  async createPlaylist(name, uris)  {
+  async createPlaylist(name, uris) {
     try {
       const headers = { Authorization: `Bearer ${Spotify.getAccessToken()}` };
-      const resUser = await fetch("https://api.spotify.com/v1/me?id", { headers: headers })
+      const resUser = await fetch("https://api.spotify.com/v1/me?id", {
+        headers: headers,
+      });
       const jsonUser = await resUser.json();
       const userId = jsonUser.id;
 
-      const resPlaylist = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+      const resPlaylist = await fetch(
+        `https://api.spotify.com/v1/users/${userId}/playlists`,
+        {
           headers: headers,
-          method: 'POST',
+          method: "POST",
           body: JSON.stringify({
-            name: name
-          })
-      });
+            name: name,
+          }),
+        }
+      );
       const jsonPlaylist = await resPlaylist.json();
       const playlistId = jsonPlaylist.id;
-      const resAddTracks = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
-        headers: headers,
-        method: 'POST',
-        body: JSON.stringify({
-          uris: uris
-        })
-      });
-      return await resAddTracks.json();  
-    } catch(error) {
+      const resAddTracks = await fetch(
+        `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+        {
+          headers: headers,
+          method: "POST",
+          body: JSON.stringify({
+            uris: uris,
+          }),
+        }
+      );
+      return await resAddTracks.json();
+    } catch (error) {
       console.log(error);
     }
   },
@@ -78,17 +86,21 @@ export const Spotify = {
   async getPlaylists() {
     try {
       const headers = { Authorization: `Bearer ${Spotify.getAccessToken()}` };
-      const response = await fetch("https://api.spotify.com/v1/me/playlists", { headers: headers })
+      const response = await fetch("https://api.spotify.com/v1/me/playlists", {
+        headers: headers,
+      });
       const jsonResponse = await response.json();
-      if(jsonResponse) {
-        return jsonResponse.items.map(item => ({
-            name: item.name,
-            image: item.images[0]?.url ?? process.env.PUBLIC_URL + 'img/album.png',
-            id: item.id
-          }));
+      if (jsonResponse) {
+        return jsonResponse.items?.map((item) => ({
+          name: item.name,
+          image: item.images
+            ? item.images[0].url
+            : `${process.env.PUBLIC_URL}img/album.png`,
+          id: item.id,
+        }));
       }
       return [];
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
   },
@@ -96,18 +108,21 @@ export const Spotify = {
   async getPlaylistDetails(id) {
     try {
       const headers = { Authorization: `Bearer ${Spotify.getAccessToken()}` };
-      const response = await fetch(`https://api.spotify.com/v1/playlists/${id}/tracks?limit=20&fields=items.track(name, album.name, artists.name)`, { headers: headers });
+      const response = await fetch(
+        `https://api.spotify.com/v1/playlists/${id}/tracks?limit=20&fields=items.track(name, album.name, artists.name)`,
+        { headers: headers }
+      );
       const jsonResponse = await response.json();
-      if(jsonResponse) {
-        return jsonResponse.items.map(item => ({
-            name: item.track.name,
-            artist: item.track.artists[0].name,
-            album: item.track.album.name
-          }));
+      if (jsonResponse) {
+        return jsonResponse.items.map((item) => ({
+          name: item.track.name,
+          artist: item.track.artists[0].name,
+          album: item.track.album.name,
+        }));
       }
       return [];
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
-  }
+  },
 };
